0490f53b7f5c51c8e850cbff9a888b4ebaffa8d2
ariadiprana <ari.adiprana@gmail.com>
2016-10-20 16:10:19 +0700

    add menu page

--- client/routes/index.js
+++ client/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/order', function(req, res, next) {
+  res.render('order');
+});
+
 module.exports = router;
